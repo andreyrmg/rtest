d871c350506071da407ce9b27dd1f4077b03ac08
Andrey Manzharov <andreyrmg@gmail.com>
2014-11-13 14:03:52 +0300

    поиск тестов

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate regex;
+use regex::Regex;
+
 use std::io::{IoError,TempDir};
+use std::io::fs;
 use std::io::fs::PathExtensions;
 use std::io::process::{Command,ProcessOutput};
 use std::os;
@@ -40,6 +44,39 @@ impl Compiler {
     }
 }
 
+struct Problem {
+    input_regex: String,
+    output_rep: String
+}
+
+struct ProblemTest {
+    input: Path,
+    answer: Path,
+}
+
+impl Problem {
+    fn find_tests(&self, dir: &Path) -> Vec<ProblemTest> {
+        if !dir.is_dir() {
+            panic!("{} not a dir", dir.display());
+        }
+        let test_re = Regex::new(self.input_regex.as_slice()).unwrap();
+        match fs::readdir(dir) {
+            Ok(contents) => {
+                let tests = contents.iter()
+                        .filter(|&path| path.is_file())
+                        .map(|path| path.filename_str().unwrap())
+                        .filter(|&s| test_re.is_match(s))
+                        .collect::<Vec<&str>>();
+                for p in tests.iter() {
+                    println!("{}", p);
+                }
+                vec![]
+            },
+            Err(e) => panic!("cannot read directory: {}", dir.display()),
+        }
+    }
+}
+
 fn main() {
     println!("The current directory: {}", os::getcwd().display());
 
@@ -58,17 +95,24 @@ fn main() {
         build_args: vec!["-So".to_string(), "-XS".to_string()],
         build_arg_working_dir: "-FE".to_string(),
     };
-    match c.compile(&abs_source_path, working_dir.path()) {
-        Success(binary) => println!("Binary: {}", binary.display()),
-        Error(process_output) => {
-            let output = from_utf8(process_output.output.as_slice()).unwrap_or("Invalid UTF-8 sequence");
-            let error = from_utf8(process_output.error.as_slice()).unwrap_or("Invalid UTF-8 sequence");
-            println!("Compilation error.");
-            println!("status: {}", process_output.status);
-            println!("output:\n{}", output);
-            println!("error:\n{}", error);
-        },
-        Failed(e) => panic!("compiler failed: {}", e),
+    // match c.compile(&abs_source_path, working_dir.path()) {
+    //     Success(binary) => println!("Binary: {}", binary.display()),
+    //     Error(process_output) => {
+    //         let output = from_utf8(process_output.output.as_slice()).unwrap_or("Invalid UTF-8 sequence");
+    //         let error = from_utf8(process_output.error.as_slice()).unwrap_or("Invalid UTF-8 sequence");
+    //         println!("Compilation error.");
+    //         println!("status: {}", process_output.status);
+    //         println!("output:\n{}", output);
+    //         println!("error:\n{}", error);
+    //     },
+    //     Failed(e) => panic!("compiler failed: {}", e),
+    // };
+
+    let p = Problem {
+        input_regex: r"(?i)(?P<test>\d+).in".to_string(),
+        output_rep: "$test.out".to_string(),
     };
+
+    p.find_tests(&Path::new("/Users/andrey/Desktop/Gorod/A/tests"));
 
 }
